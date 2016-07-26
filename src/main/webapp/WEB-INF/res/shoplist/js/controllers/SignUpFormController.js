70ff0e958085c1217f8bf2ef35207451e6d893ab
Dmytro Romenskyi <d.romenskyi@gmail.com>
2016-07-26 20:52:04 +0300

    added skeleton for SignUpFormController

---
+++ src/main/webapp/WEB-INF/res/shoplist/js/controllers/SignUpFormController.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function SignUpFormController (signUpForm) {
+	var form = signUpForm;
+}
+
+SignUpFormController.prototype
+	.checkPasswordsEqual = function() {
+	// body...
+	};
+
+SignUpFormController.prototype
+	.toggleSecondPasswordAccessibility = function() {
+	// body...
+	};
+
+SignUpFormController.prototype
+	.submitForm = function() {
+	// body...
+	};
