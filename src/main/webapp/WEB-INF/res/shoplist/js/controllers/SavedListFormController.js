170a709d6bdc2d2b2cac0acce93fe405d23c4852
Dmytro Romenskyi <d.romenskyi@gmail.com>
2016-07-26 20:32:54 +0300

    SavedListFormController.buyList() implemented

--- src/main/webapp/WEB-INF/res/shoplist/js/controllers/SavedListFormController.js
+++ src/main/webapp/WEB-INF/res/shoplist/js/controllers/SavedListFormController.js
@@ -46,5 +46,36 @@ SavedListFormController.prototype
 SavedListFormController.prototype
 	.buyList = function (listForm, button) {
 
+		$('.panel', listForm).removeClass('panel-success');
+		$('.panel', listForm).addClass('panel-default');
+
+		var list = new List();
+
+		list.key = listForm.attr('id');
+		list.bought = $('.panel', listForm).hasClass('panel-default');
+		list.owner = IS_ANON? -1: CURRENT_USER;
+		list.anonymousOwner = CURRENT_ANON_USER;
+
+		$(SAVED_PRODUCT_CLASS, listForm).each(function(index, item) {
+			var productName = $('.sl-product-name', item).text();
+			var product = new Product(productName);
+			product.key = targetProductId;
+			product.bought = $(item).hasClass('sl-bought-product');
+			list.content.push(product);
+		});
+
+		if (list.content.length === 0) {
+			return;
+		}
+
+		listService.updateList(list);
+			.done(function(data) {
+				$(ALERT_SUCCESS).text('Successfully');
+				$(ALERT_SUCCESS).toggleClass('hidden');
+			})
+			.fail(function(jqXHR, textStatus, errorThrown) {
+				$(ALERT_DANGER).text(errorThrown);
+				$(ALERT_DANGER).toggleClass('hidden');
+			})
 	};
 
